7d17c81c4c1e0bfb47fba053dd4df2ceaa4006b1
Steve M <smagara@hotmail.com>
2025-01-26 13:45:48 -0500

    set dummy MySQL connection string in injector

--- Program.cs
+++ Program.cs
@@ -40,7 +40,7 @@ var host = new HostBuilder()
         services.AddSingleton<IAzureSecrets, AzureSecrets>();
         services.AddDbContext<ContactDbContext>((serviceProvider, options) =>
         {
-            var connectionString = "to be overridden";
+            var connectionString = "Server=myServerAddress;Port=myPort;Database=myDataBase;Uid=myUsername;Pwd=;";
             var dbVersion = new MySqlServerVersion( "8.0.25");
             options.UseMySql(connectionString, dbVersion);
         });
